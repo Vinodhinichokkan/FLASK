f1c0af0d5b4fc33a38fa41012715474cd3acd903
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2025-01-06 14:35:20 +0530

    exe

---
+++ two.py
@@ -0,0 +1,16 @@
+#USING HTML TEMPLATES
+
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", content=name, r=2)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port = 5000)
+
+
+
