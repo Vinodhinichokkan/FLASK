97f2cce732f91c1ed44f1e6b37670d6dc3ddc61d
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2025-01-07 12:53:58 +0530

    exe

--- four.py
+++ four.py
@@ -1,16 +1,11 @@
-#USING HTML TEMPLATES
-
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index1.html", content="Testing", r=2)
+    return render_template("index1.html",content="Testing ")
 
 
 if __name__ == "__main__":
     app.run(debug=True,port = 5000)
-
-
-
